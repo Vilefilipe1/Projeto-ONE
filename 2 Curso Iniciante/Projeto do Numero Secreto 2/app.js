f2b570007362a0d8b720a625e63ab67484b27b94
Filipe C <30263686+Vilefilipe1@users.noreply.github.com>
2024-01-20 17:15:18 -0300

    2.09

---
+++ 2 Curso Iniciante/Projeto do Numero Secreto 2/app.js
@@ -0,0 +1,5 @@
+let textoPrincipal = document.querySelector('h1');
+textoPrincipal.innerHTML = "Jogo do Número Secreto";
+
+let ParagrafoPrincipal = document.querySelector('p');
+ParagrafoPrincipal.innerHTML = "Escolha um número entre 0 e 10.";
